629e01fc69dabde56fbba94b83cb6dcec1e27221
Paulo <paulogaspar404@hotmail.com>
2015-09-28 14:15:58 -0300

    Added the project build

---
+++ desktop/src/com/paulogaspar/ninja/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.paulogaspar.ninja.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.paulogaspar.ninja.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
