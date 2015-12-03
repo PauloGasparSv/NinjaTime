f7a30cdf620aa1b673ea946d215cd7fe45a31352
Paulo <paulogaspar404@hotmail.com>
2015-12-03 18:04:06 -0200

    Changed jar

--- core/src/com/paulogaspar/ninja/tools/KeyCombo.java
+++ core/src/com/paulogaspar/ninja/tools/KeyCombo.java
@@ -43,7 +43,7 @@ public class KeyCombo {
 	}
 	
 	public boolean update(Controller c){
-		
+		System.out.println(current);
 		if(done && playonce)return true;
 		if(timer != -1 && System.currentTimeMillis() - timer > 500){
 			current = "";
